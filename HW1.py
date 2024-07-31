3af2813bea14860992c6b5879151da1a02af9209
Henry1Tran <henrytran0309@gmail.com>
2024-08-01 06:31:36 +0700

    Henry
    Code

---
+++ HW1.py
@@ -0,0 +1,26 @@
+# Bai 1
+# input1= int(input("Input a number:"))
+# count = 0 
+
+# for i in range(1,input1+1):
+#     if input1 % i == 0:
+#         count += 1
+# if count == 2:
+#     print("Prime")
+# else: 
+#     print("Not Prime")
+
+#Bai 2
+# input1 = int(input("Input a number:"))
+# list = [1]
+# count = 0 
+
+# for i in range(1,200+1): 
+#    count = 0
+#    for j in range(1,i+1):
+#       if i % j == 0:
+#          count += 1
+#    if count == 2:
+#     list.append(i)
+
+# print(list[0:input1])
